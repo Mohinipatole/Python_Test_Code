0785f9ada63611b7b487fb8bf581389c029d52b6
Mininath Nighot <mininath.nighot9044@gmail.com>
2022-04-16 14:29:03 +0530

    files added

---
+++ hi.py
@@ -0,0 +1,282 @@
+Python 2.7.14 (v2.7.14:84471935ed, Sep 16 2017, 20:19:30) [MSC v.1500 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> #chapter 2 prctices set
+>>> # que 1: write python program to add two numbers.
+>>> a =10
+>>> b =20
+#hi
+>>> print("addition of two no a & b", a+b)
+('addition of two no a & b', 30)
+>>> 
+>>> 
+>>> # que 2: write a python program to find reminder when a number is divided by 2
+>>> a=30
+>>> b=20
+>>> print("the value of a & b is",a/b)
+('the value of a & b is', 1)
+>>> a=30
+>>> b=15
+>>> print("the reminder of a & b is", a/b)
+('the reminder of a & b is', 2)
+>>> a=17
+>>> b=3
+>>> print("the reminder of a & b is", a/b)
+('the reminder of a & b is', 5)
+>>> 
+>>> 
+>>> # que 2: write a python program to find reminder when a number is divided by 2
+>>> 
+>>> a=30
+>>> b=20
+>>> print("the remindar when a number divisible by 2 a & b",a%b)
+('the remindar when a number divisible by 2 a & b', 10)
+>>> a=50
+>>> b=14
+>>> print("the remindar of the number a & b is",a%b)
+('the remindar of the number a & b is', 8)
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> # que 3: check the type of the variable assigned using input function
+>>> 
+>>> 
+>>> 
+>>> 
+>>> # input function
+>>> a = input(" enter what is your name")
+ enter what is your name mohini
+
+Traceback (most recent call last):
+  File "<pyshell#44>", line 1, in <module>
+    a = input(" enter what is your name")
+  File "<string>", line 1, in <module>
+NameError: name 'mohini' is not defined
+>>>   a=input("enter your name")
+  
+  File "<pyshell#45>", line 2
+    a=input("enter your name")
+    ^
+IndentationError: unexpected indent
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> # que 3: check the type of the variable assigned using input function
+>>>  a = input("Enter your name")
+SyntaxError: invalid syntax
+>>> type(a)
+<type 'int'>
+>>> a=input("enter your name")
+enter your namemohni
+
+Traceback (most recent call last):
+  File "<pyshell#57>", line 1, in <module>
+    a=input("enter your name")
+  File "<string>", line 1, in <module>
+NameError: name 'mohni' is not defined
+>>> python
+
+Traceback (most recent call last):
+  File "<pyshell#58>", line 1, in <module>
+    python
+NameError: name 'python' is not defined
+>>> 
+>>> 
+
+
+>>> 
+
+>>> 
+
+
+>>> 
+
+>>> 
+
+
+>>> 
+
+>>> 
+
+>>> 
+
+
+>>> # que 4:
+>>> # que 4: use comparision operator to find out whether a given variable a is grater than b or not take a=34 b=80
+>>> a=34
+>>> b=80
+>>> 
+>>> 
+>>> 
+>>>  # que 4: use comparision operator to find out whether a given variable a is grater than b or not take a=34 b=80
+ 
+>>> a=34
+>>> a=80
+>>> a=(34>80)
+>>> print(a)
+False
+>>> # or
+>>> a=34
+>>> b=80
+>>> a(34<80)
+
+Traceback (most recent call last):
+  File "<pyshell#83>", line 1, in <module>
+    a(34<80)
+TypeError: 'int' object is not callable
+>>> a=34
+>>> b=80
+>>> a=(34<80)
+>>> print(a)
+True
+>>> a=34
+>>> b=80
+>>> a=(34=80)
+SyntaxError: invalid syntax
+>>> a=34
+>>> b=80
+>>> a=(34!=80)
+>>> print(a)
+True
+>>> a=34
+>>> b=80
+>>> a=(34==80)
+>>> print(a)
+False
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> # que 5: write a python program to find average of two numbers enterd by the user
+>>> a=30
+>>> b=20
+>>> print("the avg of two numbers a & b", a+b/2)
+('the avg of two numbers a & b', 40)
+>>> a= 80
+>>> b=30
+>>> print("the avg of two numbers a & b",a+b/2)
+('the avg of two numbers a & b', 95)
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> int a
+SyntaxError: invalid syntax
+>>> int a()
+SyntaxError: invalid syntax
+>>> int a(a)
+SyntaxError: invalid syntax
+>>> int=a
+>>> int=b
+>>> print("enter the avg value of two numbers a & b",a+b/2)
+('enter the avg value of two numbers a & b', 95)
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> # que 6: write a python program to calculate square of a number enter by the users
+>>> int=a
+>>> print("enter the square number ")
+enter the square number 
+>>> 9
+9
+>>> int=a
+>>> print("enter the square number of a",a)
+('enter the square number of a', 80)
+>>> a=16
+>>> print("enter the square of a",a)
+('enter the square of a', 16)
+>>> ('enter the square of a', 16)
+('enter the square of a', 16)
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>>  # que 6: write a python program to calculate square of a number enter by the users
+
+>>> 
+>>> 
+>>> 
+>>> 
+>>> a=input("entered the number:")
+entered the number:
+
+Traceback (most recent call last):
+  File "<pyshell#157>", line 1, in <module>
+    a=input("entered the number:")
+  File "<string>", line 0
+    
+    ^
+SyntaxError: unexpected EOF while parsing
+>>> 
+>>> 
+>>> 
+>>> a=int(a)
+
+Traceback (most recent call last):
+  File "<pyshell#161>", line 1, in <module>
+    a=int(a)
+TypeError: 'int' object is not callable
+>>> 
+>>> 
+>>> # que 6: write a python program to calculate square of a number enter by the users
+
+
+>>> int=a
+>>> a=input("enter the number")
+enter the number
+
+Traceback (most recent call last):
+  File "<pyshell#166>", line 1, in <module>
+    a=input("enter the number")
+  File "<string>", line 0
+    
+    ^
+SyntaxError: unexpected EOF while parsing
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> int=a
+>>> squ=a*a
+>>> a=int(a)
+
+Traceback (most recent call last):
+  File "<pyshell#175>", line 1, in <module>
+    a=int(a)
+TypeError: 'int' object is not callable
+>>> int=a
+>>> squ=a*a
+>>> print("the squ of a is",a)
+('the squ of a is', 16)
+>>> 
